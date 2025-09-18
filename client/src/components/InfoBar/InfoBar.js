745a967e57b833155e80570e771eba58b843781e
Mradul Mishra <mmradul203@gmail.com>
2025-09-18 07:42:41 +0530

    Initial commit - Chat-Application

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <div className="onlineIcon"></div>
+      <h3 className="roomText">Conference: {room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img className="closeIcon" src={closeIcon} alt="close icon" />
+      </a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
